1a35ca028151a0720b5a5f9d1a85b18c09bb3da1
Wes Roach <wesr000@gmail.com>
2020-12-07 18:36:50 -0600

    Add structs

---
+++ structs/structs.go
@@ -0,0 +1,14 @@
+package main
+
+// Person defines the properties of a Person
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	// i := []int{0, 1, 3}
+	// for x := range i {
+	// 	fmt.Print(x, "\n")
+	// }
+}
